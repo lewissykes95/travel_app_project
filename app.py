6ac1706731ff72f50cd1f304dc93c5b4856f0ad1
Lewis <lewisdsykes@gmail.com>
2022-07-15 09:34:26 +0100

    added code to app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-# from flask import Flask, render_template 
+from distutils.log import debug
+from flask import Flask, render_template 
+
+from controllers.destination_controller import destination_blueprint
+from controllers.traveller_controller import traveller
+
+app = Flask(__name__)
+
+app.register_blueprint(destination_blueprint)
+app.register_bluprint(traveller_blueprint)
+
+@app.route('/'
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__'
+    app.run(debug=True)

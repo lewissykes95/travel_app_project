f1f26fe0d3ad0a1c51b0cf633e6dfbced5c9e177
Lewis <lewisdsykes@gmail.com>
2022-07-21 18:36:24 +0100

    added tables destinations and bucket-list

--- db/travel_library.sql
+++ db/travel_library.sql
@@ -16,6 +16,18 @@ CREATE TABLE destinations (
     traveller_id INT NOT NULL REFERENCES travellers(id)
 );
 
+-- CREATE TABLE destinations (
+--     id SERIAL PRIMARY KEY, 
+--     name VARCHAR(255),
+--     country_id SERIAL NOT NULL REFERENCES country(id)
+-- );
+
+-- CREATE TABLE bucket-list (
+--     id SERIAL PRIMARY KEY,
+--     traveller_id SERIAL NOT NULL REFERENCES traveller(id),
+--     destination_id SERIAL NOT NULL REFERENCES destination(id),
+--     visited BOOLEAN
+-- );
 
 
 

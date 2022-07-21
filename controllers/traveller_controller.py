8ad3a2abe47d228e239c236a608213fce046361f
Lewis <lewisdsykes@gmail.com>
2022-07-21 15:09:06 +0100

    can add guests

--- controllers/traveller_controller.py
+++ controllers/traveller_controller.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
+from models.destination import Destination
 from models.traveller import Traveller
 import repositories.traveller_repository as traveller_repository
 import repositories.destination_repository as destination_repository
@@ -14,14 +15,24 @@ def login():
 def about():
     return render_template("about.html")
 
+#CREATE
 
 @travellers_blueprint.route("/login", methods=['POST'])
-def create_destination():
+def create_guest():
     name            = request.form['name']
     age             = request.form['age']
     traveller       = Traveller (name, age)
     traveller       = traveller_repository.save(traveller)
     return redirect('/login')
+
+#DELETE
+
+# @travellers_blueprint.route("/travellers/<id>/delete", methods=['POST'])
+# def delete_traveller(id):
+#     traveller_repository.delete(id)
+#     return redirect ('/destinations/new')
+
+
 
 
 
